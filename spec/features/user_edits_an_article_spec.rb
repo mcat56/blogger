349ec022accc79098598baaeba4dcbb56fb7f2df
Mary Lang <mcl86@cornell.edu>
2019-09-29 15:06:23 -0600

    Add edit and update method test passing

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 describe 'user edits an article' do
-  describe 'they link from show page' do
-    describe 'they edit the title and or body' do
-      it 'displays the changes' do
+  describe 'they link from a show page' do
+    describe 'they fill in an edit field and submit' do
+      it 'displays the updated information on a show' do
         article_1 = Article.create!(title: "Title 1", body: "Body 1")
         article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
         visit article_path(article_1)
         click_link "Edit"
 
-        expect(current_path).to eq(new_article_path)
+        fill_in "article[title]", with: "Different Title"
+        fill_in "article[body]", with: "Different Body"
+        click_on "Update Article"
 
-        fill_in "article[title]", with: "New Title!"
-        fill_in "article[body]", with: "New Body!"
-        click_on "Submit Changes"
-
-        expect(page).to have_content("New Title!")
-        expect(page).to have_content("New Body!")
+        expect(current_path).to eq(article_path(article_1))
+        expect(page).to have_content("Different Title")
+        expect(page).to_not have_content("Title 1")
+        expect(page).to have_content("Different Body")
+        expect(page).to_not have_content("Body 1")
+        # expect(page).to have_content("Article Different Title Updated!")
       end
     end
   end
