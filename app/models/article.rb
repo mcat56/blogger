cca396643d1b290aa71e73f5a69586491a89d1ba
Mary Lang <mcl86@cornell.edu>
2019-10-17 16:58:58 -0600

    Added gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,9 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
 
+  has_attached_file :image, styles: { medium: '250x250', thumb: '100x100'}
+  validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'iamge/png']
+
   def tag_list
     tags.join(", ")
   end
